da7c982ec1b8e03e4459a0dd403138f685d3a0fe
mpopov <mpopov@neoflex.ru>
2022-02-04 21:53:20 +0300

    Add append, flatmap

--- src/main/scala/chapter5/StreamImpl.scala
+++ src/main/scala/chapter5/StreamImpl.scala
@@ -8,21 +8,46 @@ object StreamImpl extends App {
     def isEmpty: Boolean = uncons.isEmpty
     def toList: List[A] =
       uncons match {
-        case Some(value) => List(value._1) ::: value._2.toList
-        case None        => List()
+        case Some((h, t)) => List(h) ::: t.toList
+        case None         => List()
       }
     def take(n: Int): MyStream[A] =
       uncons match {
-        case Some(value) => if (n > 0) cons(value._1, value._2.take(n - 1)) else empty
-        case None        => empty
+        case Some((h, t)) => if (n > 0) cons(h, t.take(n - 1)) else empty
+        case None         => empty
       }
 
     def takeWhile(p: A => Boolean): MyStream[A] =
       uncons match {
-        case Some(value) => if (p(value._1)) cons(value._1, value._2.takeWhile(p)) else empty
-        case None        => empty
+        case Some((h, t)) => if (p(h)) cons(h, t.takeWhile(p)) else empty
+        case None         => empty
+      }
+    def foldRight[B](z: => B)(f: (A, => B) => B): B =
+      uncons match {
+        case Some((h, t)) => f(h, t.foldRight(z)(f))
+        case None         => z
       }
 
+    def exists(p: A => Boolean): Boolean =
+      foldRight(false)((a, b) => p(a) || b)
+
+    def forAll(p: A => Boolean): Boolean =
+      foldRight(true)((a, b) => p(a) && b)
+
+    def takeWhile2(p: A => Boolean): MyStream[A] =
+      foldRight(empty[A])((a, b) => if (p(a)) cons(a, b) else empty)
+
+    def map[B](f: A => B): MyStream[B] =
+      foldRight(empty[B])((a, b) => cons(f(a), b))
+
+    def filter(p: A => Boolean): MyStream[A] =
+      foldRight(empty[A])((el, str) => if (p(el)) cons(el, str) else str)
+
+    def append[B >: A](s: => MyStream[B]): MyStream[B] =
+      foldRight(s)((el1, str1) => cons(el1, str1))
+
+    def flatMap[B](f: A => MyStream[B]): MyStream[B] =
+      foldRight(empty[B])((el, str) => f(el) append (str))
   }
   object MyStream {
     def empty[A]: MyStream[A] = new MyStream[A] { def uncons: Option[(A, MyStream[A])] = None }
@@ -35,6 +60,9 @@ object StreamImpl extends App {
       if (as.isEmpty) empty
       else cons(as.head, apply(as.tail: _*))
   }
+  val ones: MyStream[Int] = cons(1, ones)
+  println(ones.map(_ + 1).exists(_ % 2 == 0))
+  println(ones.takeWhile(_ == 1))
+  println(ones.forAll(_ != 1))
 
-  println(MyStream(1, 3, 3, 4, 5).take(3).toList)
 }
